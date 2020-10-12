a0271f0042551184cad3f4fa68769fea2562a412
Curder <q.curder@gmail.com>
2020-10-12 14:46:22 +0800

    change laravel-mix-purgecss version

--- src/Commands/PresetCommand.php
+++ src/Commands/PresetCommand.php
@@ -62,7 +62,7 @@ class PresetCommand extends Command
             'tailwindcss' => '^1.8.10',
             'laravel-mix' => '^5.0.5',
             'laravel-mix-tailwind' => '^0.1.0',
-            'laravel-mix-purgecss' => '^5.0.0'
+            'laravel-mix-purgecss' => '^4.0.0'
         ] + Arr::except($packages, [
             'bootstrap',
             'popper.js',
